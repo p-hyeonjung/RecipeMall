39524aca324324544d931814e72dac24e12a0787
Lee-JooHan <121008798+Lee-JooHan@users.noreply.github.com>
2023-05-11 20:58:34 +0900

    Add files via upload

--- js/product_info.js
+++ js/product_info.js
@@ -1,9 +1,17 @@
 $(function(){
     $('.product_count_minus').on('click',function(){
         minus();
+        $('.option2').html($('.count').val())
     });
     $('.product_count_plus').on('click',function(){
         plus();
+        $('.option2').html($('.count').val())
+    });
+    $('.product_date_select').change(function(){
+        option1();
+    })
+    $('.regular_order').change(function (){
+        option3();
     });
 });
 function minus(){
@@ -15,4 +23,14 @@ function minus(){
 function plus(){
     let num=parseInt($('.product_count input').val());
     $('.product_count input').val(num+1);
+}
+function option1(){
+    $('.option1').html($('.product_date_select').val());
+}
+function option3(){
+    if($('.regular_order').is(":checked")){
+        $('.option3').html("O");
+    }else{
+        $('.option3').html("X");
+    };
 }
