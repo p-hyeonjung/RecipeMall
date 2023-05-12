2d52d8481a503e8faad399f9ca79302c9b2b6861
p-hyeonjung <phjung1811@gmail.com>
2023-05-12 19:08:16 +0900

    faq accordion 기능 추가

--- js/mypage.js
+++ js/mypage.js
@@ -1,6 +1,17 @@
 $(function () {
     // 자주하는 질문 accordion
-    $('.accordion_title').click(function () {
+    $('.accordion_title').on('click', function() {
 
-    });
+        function slideDown(target) {
+          slideUp();
+          $(target).addClass('on').next().slideDown();
+        }
+      
+        function slideUp() {
+          $('.accordion_title').removeClass('on').next().slideUp();
+        }
+      
+        $(this).hasClass('on') ? slideUp() : slideDown(this);
+        console.log(event.target.className);
+      });
 });
