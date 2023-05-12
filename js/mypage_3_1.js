f0ffae3c49f325d320abc042316f48f108381a73
usa112233 <58211138+usa112233@users.noreply.github.com>
2023-05-12 16:06:22 +0900

    slideUp slideDown(펼쳐보기) 추가

--- js/mypage_3_1.js
+++ js/mypage_3_1.js
@@ -1,5 +1,5 @@
 $(function(){
-    $('.product_open').on('click', function(){
+    $('.btn_product').on('click', function(){
         let reviewId = $(this).data('review-id');
         slide_review(reviewId);
     });
@@ -8,9 +8,12 @@ $(function(){
 function slide_review(reviewId){
     let reviewForm = $(`#review_form_${reviewId}`);
     reviewForm.slideUp();
+    let productOpenButton = $(`.btn_product[data-review-id="${reviewId}"]`);
     if(reviewForm.is(':hidden')){
         reviewForm.slideDown();
+        productOpenButton.text('줄이기');
     } else {
         reviewForm.slideUp();
+        productOpenButton.text('펼쳐보기');
     }
 }
