31f45058486537d19be23ea917137ecf2465baf0
jungwL <121008837+jungwL@users.noreply.github.com>
2023-05-12 21:15:45 +0900

    Add files via upload

--- js/mypage.js
+++ js/mypage.js
@@ -1,17 +1,6 @@
 $(function () {
     // 자주하는 질문 accordion
-    $('.accordion_title').on('click', function() {
+    $('.accordion_title').click(function () {
 
-        function slideDown(target) {
-          slideUp();
-          $(target).addClass('on').next().slideDown();
-        }
-      
-        function slideUp() {
-          $('.accordion_title').removeClass('on').next().slideUp();
-        }
-      
-        $(this).hasClass('on') ? slideUp() : slideDown(this);
-        console.log(event.target.className);
-      });
+    });
 });
