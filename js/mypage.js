2eb70596abb4c957b396ebede6f0af0c61da5642
p-hyeonjung <phjung1811@gmail.com>
2023-05-12 21:40:44 +0900

    post check func add

--- js/mypage.js
+++ js/mypage.js
@@ -13,5 +13,27 @@ $(function () {
       
         $(this).hasClass('on') ? slideUp() : slideDown(this);
         console.log(event.target.className);
-      });
+    });
+    
+    /* ------------------------- mypage post/comment ---------------------------- */
+    // 체크박스
+    let chk;
+
+    $('#post_chk_all').click(function () {
+        chk=$(this).is(':checked');
+        chk_all(chk);
+    });
+
+    $('.check').click(function () {
+        $('#post_chk_all').prop('checked',false);
+    });
+
+    function chk_all(chk) {
+        if(chk) {
+            $('.check').prop('checked',true); 
+        } else {    
+            $('.check').prop('checked',false); 
+        }
+    }
+
 });
