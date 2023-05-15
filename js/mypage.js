85afcb58e2fdbfb1c87d51909861264743058892
p-hyeonjung <phjung1811@gmail.com>
2023-05-15 18:45:15 +0900

    faq slideDown/Up image add

--- js/mypage.js
+++ js/mypage.js
@@ -5,34 +5,14 @@ $(function () {
         function slideDown(target) {
           slideUp();
           $(target).addClass('on').next().slideDown();
+          $(target).find('.fa').find('i').removeClass('fa-chevron-up');
+          $(target).find('.fa').find('i').addClass('fa-chevron-down');
         }
       
         function slideUp() {
           $('.accordion_title').removeClass('on').next().slideUp();
-        }
-      
-        $(this).hasClass('on') ? slideUp() : slideDown(this);
-        console.log(event.target.className);
-    });
-    
-    /* ------------------------- mypage post/comment ---------------------------- */
-    // 체크박스
-    let chk;
-
-    $('#post_chk_all').click(function () {
-        chk=$(this).is(':checked');
-        chk_all(chk);
-    });
-
-    $('.check').click(function () {
-        $('#post_chk_all').prop('checked',false);
-        function slideDown(target) {
-          slideUp();
-          $(target).addClass('on').next().slideDown();
-        }
-      
-        function slideUp() {
-          $('.accordion_title').removeClass('on').next().slideUp();
+          $('.fa').find('i').removeClass('fa-chevron-down');
+          $('.fa').find('i').addClass('fa-chevron-up');
         }
       
         $(this).hasClass('on') ? slideUp() : slideDown(this);
@@ -56,17 +36,18 @@ $(function () {
         post_chk=$(this).is(':checked');
         chk_all(post_chk);
     });
-
-    $('.chk').click(function () {
-        $('#post_chk_all').prop('checked',false);
-        $('#comment_chk_all').prop('checked',false);
-    });
+    
     // comment check
     $('#comment_chk_all').click(function () {
         comm_chk=$(this).is(':checked');
         chk_all(comm_chk);
     });
     
+    $('.chk').click(function () {
+        $('#post_chk_all').prop('checked',false);
+        $('#comment_chk_all').prop('checked',false);
+    });
+
     /* ------------- 글/댓글 게시판 메뉴 분류 css 유지 ---------- */
     let item=$('.menu_li');
 
@@ -74,13 +55,5 @@ $(function () {
         $(this).addClass('active');
         item.not($(this)).removeClass('active');
     });
-
-    function chk_all(chk) {
-        if(chk) {
-            $('.check').prop('checked',true); 
-        } else {    
-            $('.check').prop('checked',false); 
-        }
-    }
 
 });
